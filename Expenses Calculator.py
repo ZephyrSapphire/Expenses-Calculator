acd27b346f0381d939120ae5fa0f3947afd66cf0
Alex <billyalexandersetyohandojo45@gmail.com>
2026-06-06 13:59:07 +0700

    Updated Calculator + Long Term Storage

--- Expenses Calculator.py
+++ Expenses Calculator.py
@@ -1,6 +1,8 @@
 # Expenses Calculator
 from tabulate import tabulate
-Products_List = []
+import csv
+import os
+path = os.path.join(os.path.dirname(__file__), "Products.csv")
 
 def Product_Input():
     while True:
@@ -17,9 +19,18 @@ def Product_Input():
             if Product_Duration.lower() not in ["once", "daily", "weekly", "monthly", "yearly"]:
                 print("Try again")
             else:
+                Product_Duration = Product_Duration.capitalize()
                 break
         
-        Products_List.append({"Product Name" : Product_Name, "Product Price" : Product_Price, "Product Payment Period" : Product_Duration})
+        file_empty = not os.path.exists(path) or os.path.getsize(path) == 0
+
+        with open(path, "a", newline="") as Input_Products:
+            Fields = ["Product Name", "Product Price", "Product Payment Period"]
+            write = csv.DictWriter(Input_Products, fieldnames=Fields)
+            if file_empty:
+                write.writeheader()
+            write.writerow({"Product Name" : Product_Name, "Product Price" : Product_Price, "Product Payment Period" : Product_Duration})
+
         while True:
             Loop = str(input("Would you like to input again? (Y/N) : "))
             if Loop.lower() == "y":
@@ -30,92 +41,141 @@ def Product_Input():
                 print("Try again")
         
 def Product_And_Price():
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    Products_List = []
+    with open(path, "r") as Products_Info:
+        read = csv.DictReader(Products_Info)
+        for Products in read:
+           Products_List.append(Products)
+    if not Products_List:
+        print("There's nothing to show!")
+    else:
         print(tabulate(Products_List, headers="keys", tablefmt="grid"))
 
 def Product_Total_Price():
     Total = 0
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    Products_List = []
+    with open(path, "r") as Products_Info:
+        read = csv.DictReader(Products_Info)
+        for product in read:
+            Products_List.append(product)
+
     for products in Products_List:
         daily_payment = 0
         if products["Product Payment Period"].lower() == "once":
             continue
         elif products["Product Payment Period"].lower() == "daily":
-            daily_payment = products["Product Price"]
+            daily_payment = float(products["Product Price"])
         elif products["Product Payment Period"].lower() == "weekly":
-            daily_payment = products["Product Price"] / 7
+            daily_payment = float(products["Product Price"]) / 7
         elif products["Product Payment Period"].lower() == "monthly":
-            daily_payment = products["Product Price"] / 30
+            daily_payment = float(products["Product Price"]) / 30
         elif products["Product Payment Period"].lower() == "yearly":
-            daily_payment = products["Product Price"] / 365
+            daily_payment = float(products["Product Price"]) / 365
         Total += daily_payment
     return Total
     
 def Expenses_Duration(Total):
     while True:
             Duration = input("Enter calculation period (Daily, Weekly, Monthly, Yearly) : ")
-            if Duration.lower() == "daily":
+            if Duration.lower() == "daily" or Duration.lower() == "day":
                 multiplier = 1
                 break
-            elif Duration.lower() == "weekly":
+            elif Duration.lower() == "weekly" or Duration.lower() == 'week':
                 multiplier = 7
                 break
-            elif Duration.lower() == "monthly":
+            elif Duration.lower() == "monthly" or Duration.lower() == 'month':
                 multiplier = 30
                 break
-            elif Duration.lower() == "yearly":
+            elif Duration.lower() == "yearly" or Duration.lower() =='year':
                 multiplier = 365
                 break
             else:
                 print("Try again")
 
     Calculated_Data = []
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    Products_List = []
+    with open(path, "r") as Products_Info:
+        read = csv.DictReader(Products_Info)
+        for product in read:
+            Products_List.append(product)
+
     for products in Products_List:
         daily_payment = 0
         if  products["Product Payment Period"].lower() == "once":
-            Calculated_Data.append({"Product Name" : products["Product Name"], "Total Price" : "$" + str(round(products["Product Price"]))})
+            Calculated_Data.append({"Product Name" : products["Product Name"], "Total Price" : "$" + str(round(float(products["Product Price"])))})
             continue
         elif products["Product Payment Period"].lower() == "daily":
-            daily_payment = products["Product Price"]
+            daily_payment = float(products["Product Price"])
         elif products["Product Payment Period"].lower() == "weekly":
-            daily_payment = products["Product Price"] / 7
+            daily_payment = float(products["Product Price"]) / 7
         elif products["Product Payment Period"].lower() == "monthly":
-            daily_payment = products["Product Price"] / 30
+            daily_payment = float(products["Product Price"]) / 30
         elif products["Product Payment Period"].lower() == "yearly":
-            daily_payment = products["Product Price"] / 365
+            daily_payment = float(products["Product Price"]) / 365
         Calculated_Data.append({"Product Name" : products["Product Name"], "Total Price" : "$" + str(round(daily_payment * multiplier, 2))})
 
     print(tabulate(Calculated_Data, headers="keys", tablefmt="grid"))
     Once_total = 0
     for p in Products_List:
         if p["Product Payment Period"].lower() == "once":
-            Once_total += p["Product Price"]
+            Once_total += float(p["Product Price"])
     print("Your total expenses : $" + str(round(Total * multiplier + Once_total)))
 
-def Remove_Products(Products_List):
-    for index, product in enumerate(Products_List):
-        print(f"{index + 1}. {product['Product Name']} - ${product["Product Price"]}")
+def Remove_Products():
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    New_Products_List = []
+    with open(path, "r") as Product_Info:
+        read = csv.DictReader(Product_Info)
+        for index, product in enumerate(read):
+            print(f"{index + 1}. {product['Product Name']} - ${product['Product Price']} - {product["Product Payment Period"]}")
+            New_Products_List.append(product)
+    
     while True:
         try:
             Remove = int(input("Select product number to remove : "))
-            if 1 <= Remove <= len(Products_List):
+            if 1 <= Remove <= len(New_Products_List):
                 break
             else:
-                print(f"Input a number between 1 and {len(Products_List)}")
+                print(f"Input a number between 1 and {len(New_Products_List)}")
         except ValueError:
             print("Input a number!")
     
-    Products_List.pop(Remove - 1)
-    return Remove
+    New_Products_List.pop(Remove - 1)
 
-def Change_Price(Products_List):
-    for index, product in enumerate(Products_List):
-        print(f"{index + 1}. {product['Product Name']} - ${product["Product Price"]}")
+    with open(path, "w", newline="") as Output_File:
+        fields = ["Product Name", "Product Price", "Product Payment Period"]
+        write = csv.DictWriter(Output_File, fieldnames=fields)
+        write.writeheader()
+        write.writerows(New_Products_List)
+
+def Change_Price():
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    New_Products_List = []
+    with open(path, "r") as Product_Info:
+        read = csv.DictReader(Product_Info)
+        for index, product in enumerate(read):
+            print(f"{index + 1}. {product['Product Name']} - ${product["Product Price"]}")
+            New_Products_List.append(product)
     while True:
         try:
             Change = int(input("Select product number to change price : "))
-            if 1 <= Change <= len(Products_List):
+            if 1 <= Change <= len(New_Products_List):
                 break
             else:
-                print(f"Input a number between 1 and {len(Products_List)}")
+                print(f"Input a number between 1 and {len(New_Products_List)}")
         except ValueError:
             print("Input a number!")
 
@@ -126,18 +186,31 @@ def Change_Price(Products_List):
         except ValueError:
             print("Input a number!")
 
-    Products_List[Change - 1]["Product Price"] = New_Price
+    New_Products_List[Change - 1]["Product Price"] = New_Price
 
-def Change_Expense_Period(Products_List):
-    for index, product in enumerate(Products_List):
-        print(f"{index + 1}. {product["Product Name"]} - ${product["Product Price"]}")
+    with open(path, "w", newline="") as Output_File:
+        fields = ["Product Name", "Product Price", "Product Payment Period"]
+        write = csv.DictWriter(Output_File, fieldnames=fields)
+        write.writeheader()
+        write.writerows(New_Products_List)
+
+def Change_Expense_Period():
+    if not os.path.exists(path):
+        print("There's nothing to show!")
+        return
+    New_Products_List = []
+    with open(path, "r") as Product_Info:
+        read = csv.DictReader(Product_Info)
+        for index, product in enumerate(read):
+            print(f"{index + 1}. {product["Product Name"]} - ${product["Product Payment Period"]}")
+            New_Products_List.append(product)
     while True:
         try:
             Change_Period = int(input("Select product number to change expense period : "))
-            if 1 <= Change_Period <= len(Products_List):
+            if 1 <= Change_Period <= len(New_Products_List):
                 break
             else:
-                print(f"Input a number between 1 and {len(Products_List)}")
+                print(f"Input a number between 1 and {len(New_Products_List)}")
         except ValueError:
             print("Input a number!")
 
@@ -148,27 +221,32 @@ def Change_Expense_Period(Products_List):
         else:
             break
 
-    Products_List[Change_Period - 1]["Product Payment Period"] = New_Period.lower()
+    New_Products_List[Change_Period - 1]["Product Payment Period"] = New_Period.capitalize()    
 
-Product_Input()
+    with open(path, "w", newline="") as Output_File:
+        fields = ["Product Name", "Product Price", "Product Payment Period"]
+        write = csv.DictWriter(Output_File, fieldnames=fields)
+        write.writeheader()
+        write.writerows(New_Products_List)
 
 while True:
     Features = input("What Would You Like To Do? (Add Product, Remove Product, Show Products, Change Price, Change Expense Period, Calculate Total Expenses, Quit) : ")
-    if Features.lower() == "add product":
+    if Features.lower() == "add product" or Features.lower() == "add":
         Product_Input()
-    elif Features.lower() == "remove product":
-        Remove_Products(Products_List)
-    elif Features.lower() == "show products":
+    elif Features.lower() == "remove product" or Features.lower() == "remove":
+        Remove_Products()
+    elif Features.lower() == "show products" or Features.lower() == "show":
         Product_And_Price()
     elif Features.lower() == "change price":
-        Change_Price(Products_List)
-    elif Features.lower() == 'change expense period':
-        Change_Expense_Period(Products_List)
-    elif Features.lower() == "calculate total expenses":
+        Change_Price()
+    elif Features.lower() == 'change expense period' or Features.lower() == "change expense":
+        Change_Expense_Period()
+    elif Features.lower() == "calculate total expenses" or Features.lower() == "calculate":
         Total = Product_Total_Price()
-        Expenses_Duration(Total)
+        if Total is not None:
+            Expenses_Duration(Total)
     elif Features.lower() == "quit":
-        print("Thank's for calculating, ByeBye!")
+        print("Thanks for calculating, byebye!")
         break
     else:
         print("Try again")
